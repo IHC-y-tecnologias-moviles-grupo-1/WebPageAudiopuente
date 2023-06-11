bdbc5b50b8aa49c28b51cb65da78be986019e2a3
Ridan <u202112190@upc.edu.pe>
2023-06-10 19:18:05 -0500

    Version: 0.1.0-alpha feat: Layout para celular casi terminado
    He terminado practicamente el layout para dispositivos moviles
    Ahora solo faltan 4 imagenes (2 para el hero, 2 para about), y
    al configuracion de media queries en css para hacerlo responsivo a
    dispositivos de pantallas mas grandes.

---
+++ javascript/index_script.js
@@ -0,0 +1,17 @@
+
+const bar = document.getElementById('bar')
+const navbar = document.getElementById('nav-buttons');
+const close = document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click', () =>{
+        navbar.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener('click', () =>{
+        navbar.classList.remove('active');
+    })
+}
+
